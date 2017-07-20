14ea6a95398f97f6827b18e4b8bab99c0d0dd886
nikhilgupta1611 <nikhilgupta1611@gmail.com>
2017-07-20 17:34:10 -0400

    Add files via upload

---
+++ makeCacheMatrix.R
@@ -0,0 +1,44 @@
+#  FUNCTION 1, "makeCacheMatrix" creates a special "matrix", which is
+# really a list containing a function to
+# 
+# 1.  set the value of the matrix
+# 2.  get the value of the matrix
+# 3.  set the value of the inverse
+# 4.  get the value of the inverse
+
+# FUNCTION 1
+makeCacheMatrix <- function(x = matrix()) {
+  inv<- NULL
+  set <- function(y) {
+    x <<- y
+    inv<<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+# FUNCTION 2 "cacheSolve" calculates the inverse of the special "matrix"
+# created with the above function. However, it first checks to see if the
+# inverse has already been calculated. If so, it `get`s the inverse from the
+# cache and skips the computation. Otherwise, it calculates the inverse of
+# the data and sets the value of the inverse in the cache via the `setinverse`
+# function.
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)   # SOLVE calculates the inverse for the exactly singular invertible matrices
+  x$setinverse(inv)
+  return(inv)
+}
+
+
+
